ad6a508a4b31d38a264fa3f887009201e8b9ec01
Dakintelu <151844066+Dakintelu@users.noreply.github.com>
2024-07-17 14:06:21 +0100

    ...

--- Image.py
+++ Image.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_hear_cascade = cv2.CaescadeClassifier('hearcascade_frontalface_')
-image - cv2.imread('demo.jpg')
+image - cv2.imread('photo.jfif')
 gray = cv2.cvtcolor(image, cv2.COLOR_BGR2GRAY)
 #cv2.imshow('Gray',gray)
 #cv2.waitkey()
